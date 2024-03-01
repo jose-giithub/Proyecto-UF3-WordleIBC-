c4e48dd42af16a4a1c8eac5ed061c7b4dbafe10e
jose-giithub <jose.rodriguez@insbaixcamp.cat>
2024-03-01 13:56:35 +0100

    termino funciones verificar palabra

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,6 +1,3 @@
-
-
-
 // Función para agregar letra a la celda correcta
 function agregarLetra(letraPresionada) {
   console.log("Letra presionada: " + letraPresionada);
@@ -22,20 +19,20 @@ function agregarLetra(letraPresionada) {
   }
 
   const idCeldaActual = `cell${filaActual}-${celdaActual}`;
-  const divActual = document.getElementById(idCeldaActual);
-  
-  if (divActual) {
-    divActual.innerText = letraPresionada.toUpperCase();
-    palabra+=letraPresionada;
+ 
+  const inputActual = document.getElementById(idCeldaActual);
+ 
+  if (inputActual) {
+    inputActual.value = letraPresionada.toUpperCase();
+    palabra += letraPresionada.toLowerCase();
     console.log("palabra añadir", palabra);
     celdaActual++;
   }
 }
-
 // Función para vefiriicar si el formulario está completo si exite la palabra
 
 function validarPalabra() {
-  console.log("palabra", palabra, 'palabra aleatoria', palabraAleatoria);
+  console.log("palabra", palabra, "palabra aleatoria", palabraAleatoria);
 
   if (palabra.length !== 5) {
     console.log("Debe completar las 5 letras antes de presionar Enter.");
@@ -46,55 +43,110 @@ function validarPalabra() {
     console.log("La palabra existe en el diccionario.");
     if (palabra === palabraAleatoria) {
       console.log("¡Correcto! La palabra es igual a la palabra aleatoria.");
+      window.alert("|Correcto!")
+       marcarPalabra();
       // Aquí podrías implementar alguna lógica para manejar el caso de éxito.
     } else {
-      console.log("La palabra no es la correcta, pero existe en el diccionario.");
+      console.log(
+        "La palabra no es la correcta, pero existe en el diccionario.");
+      window.alert("La palabra no es la correcta")
+      //busco coincidencia entre la palabar dada y la aleatoria
       // Preparar para la siguiente fila
+      verificarLetras(palabra);
       prepararSiguienteFila();
     }
   } else {
     console.log("La palabra no existe en el diccionario.");
+    window.alert("La palabra no existe")
     // Puedes optar por permitir al usuario intentarlo nuevamente sin avanzar de fila o forzar el avance de todos modos.
     prepararSiguienteFila();
   }
 }
 
-function prepararSiguienteFila() {
-  // Deshabilitar la fila actual (opcional)
-  const filaActualDiv = document.getElementById(`fila${filaActual}`);
-  if (filaActualDiv) {
-    filaActualDiv.classList.add("disabled"); // Asegúrate de definir el estilo .disabled en tu CSS
-  }
+//verificar letras
+function verificarLetras(palabraIngresada) {
+  for (let i = 0; i < palabraIngresada.length; i++) {
+    const letra = palabraIngresada[i];
+    const idCeldaActual = `cell${filaActual}-${i + 1}`;
+    const celdaActual = document.getElementById(idCeldaActual);
 
+    // Si la letra está en la palabra y en la posición correcta, la marcamos en verde
+    if (letra === palabraAleatoria[i]) {
+      celdaActual.classList.add("correcta"); // Agrega clase para letra en posición correcta
+    }
+    // Si la letra está en la palabra pero no en la posición correcta, la marcamos en amarillo
+    else if (palabraAleatoria.includes(letra)) {
+      celdaActual.classList.add("presente"); // Agrega clase para letra presente pero en posición incorrecta
+    }
+  }
+}
+function marcarPalabra() {
+  // Asumiendo que 'color' es una clase que ya tienes definida en tu CSS
+  for (let i = 1; i <= 5; i++) {
+    const idCeldaActual = `cell${filaActual}-${i}`;
+    const celdaActual = document.getElementById(idCeldaActual);
+
+    if (celdaActual) {
+      celdaActual.classList.add('correcta');
+    }
+  }
+  
+  // Desactivar la entrada de teclado
+  desactivarEntradaTeclado();
+}
+
+function desactivarEntradaTeclado() {
+  // Elimina el event listener de las teclas presionadas
+  document.removeEventListener('keydown', teclasPresionada);
+  
+  // Además, si tienes botones en pantalla para la entrada de teclado virtual,
+  // también deberías deshabilitarlos.
+  const botonesTeclado = document.querySelectorAll('.btn.btn-secondary.m-1');
+  botonesTeclado.forEach(function (button) {
+    button.disabled = true; // Deshabilitar los botones del teclado virtual
+  });
+}
+
+function prepararSiguienteFila() {
+  console.log("Preparando para la siguiente fila.");
+  
+  // Deshabilitar la fila actual
+  const filaActualDiv = document.getElementById(`fila${filaActual}`);
+  console.log(filaActualDiv);
+  if (filaActualDiv) {
+    filaActualDiv.classList.add("disabled"); // Añade la clase .disabled al contenedor de la fila
+    let inputs = filaActualDiv.querySelectorAll("input.cell");
+    inputs.forEach((input) => {
+      input.disabled = true; // Deshabilita los inputs de la fila actual
+    });
+  }
   // Reiniciar palabra y avanzar a la siguiente fila
   palabra = "";
-  if (filaActual < 6) { // Asumiendo que tienes un máximo de 6 filas.
+  if (filaActual < 6) {
     filaActual++;
     celdaActual = 1;
   } else {
     console.log("Juego terminado. No quedan más intentos.");
-    // Aquí puedes implementar lógica para finalizar el juego o reiniciarlo.
   }
 }
 
 function borrarUltimaLetra() {
   if (celdaActual > 1 || (filaActual > 1 && celdaActual === 1)) {
-      if (celdaActual === 1) {
-          // Ajustar si tienes lógica para manejar múltiples filas
-          filaActual -= 1;
-          celdaActual = 5; // O el número máximo de celdas por fila que tienes
-      } else {
-          celdaActual--;
-      }
+    if (celdaActual === 1) {
+      // Ajustar si tienes lógica para manejar múltiples filas
+      filaActual -= 1;
+      celdaActual = 5; // O el número máximo de celdas por fila que tienes
+    } else {
+      celdaActual--;
+    }
 
-      const idCeldaActual = `cell${filaActual}-${celdaActual}`;
-      const divActual = document.getElementById(idCeldaActual);
-      if (divActual) {
-          divActual.innerText = "";
-      }
-      // Actualizar la palabra después de ajustar la celda actual
+    const idCeldaActual = `cell${filaActual}-${celdaActual}`;
+    const inputActual = document.getElementById(idCeldaActual);
+    if (inputActual) {
+      inputActual.value = "";
       palabra = palabra.slice(0, -1);
       console.log("palabra borrar", palabra);
+    }
   }
 }
 
@@ -142,7 +194,10 @@ function teclasPresionada(event) {
   ];
   // Convertir a minúsculas para hacer la comparación y aceptar Enter y Backspace como están
   if (
-    letrasValidas.includes(teclaPresionada.toLowerCase()) || teclaPresionada === "Enter" || teclaPresionada === "Backspace") {
+    letrasValidas.includes(teclaPresionada.toLowerCase()) ||
+    teclaPresionada === "Enter" ||
+    teclaPresionada === "Backspace"
+  ) {
     //console.log("Tecla válida presionada: " + event.key);
     agregarLetra(teclaPresionada);
     // return event.key;
